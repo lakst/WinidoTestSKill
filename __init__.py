51b359df3e5ac4ec5aa6b2bd25d1709a7a91946e
Lakshay Thareja <lakshaydchamp@gmail.com>
2018-03-06 02:06:52 +0530

    TO

--- __init__.py
+++ __init__.py
@@ -11,14 +11,26 @@ class WinidoTestSkill(MycroftSkill):
         super(WinidoTestSkill, self).__init__(name="WinidoTestSkill")
 
     def initialize(self):
-        start_intent = IntentBuilder("StartIntent").require("PlayKeyWord").build()
+        start_intent = IntentBuilder("StartIntent").require("PlayKeyWord").adds_context('playing').build()
         self.register_intent(start_intent, self.handle_start_intent)
 
     def handle_start_intent(self, message):
         try:
-            self.speak_dialog("response", None, true)
+            self.speak_dialog("response.dialog", None, true)
+            self.count = 0
+            self.play=true
+            while play:
+                self.speak_dialog("question", None, true)
         except:
             self.speak_dialog("error")
+
+    @intent_handler(IntentBuilder('CorrectAnswerIntent').require("answer1KeyWord").build())
+    def answer_intent(self, message):
+        self.speak_dialog("CorrectAnswer")
+
+    @intent_handler(IntentBuilder('WrongAnswerIntent').build())
+    def answer_intent(self, message):
+        self.speak_dialog("WrongAnswer")
 
 def create_skill():
     return WinidoTestSkill()
