1c8891063d485af56a1892f8d1675a1c87181c67
Lakshay Thareja <lakshaydchamp@gmail.com>
2018-03-06 15:05:21 +0530

    L

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import getLogger
@@ -10,10 +11,7 @@ class WinidoTestSkill(MycroftSkill):
     def __init__(self):
         super(WinidoTestSkill, self).__init__(name="WinidoTestSkill")
 
-    def initialize(self):
-        start_intent = IntentBuilder("StartIntent").require("PlayKeyWord").build()
-        self.register_intent(start_intent, self.handle_start_intent)
-
+    @intent_handler(IntentBuilder("StartIntent").require("PlayKeyWord"))
     def handle_start_intent(self, message):
         self.speak_dialog("response", None, expect_response=True)
         play = True
@@ -21,14 +19,12 @@ class WinidoTestSkill(MycroftSkill):
             self.speak_dialog("question", None, expect_response=True)
             play = False
 
-    @intent_handler(IntentBuilder('CorrectAnswerIntent').require("answer1KeyWord"))
-    def answer_intent(self, message):
-        self.speak_dialog("CorrectAnswer")
-
-    @intent_handler(IntentBuilder('WrongAnswerIntent'))
-    def answer_intent(self, message):
-        self.speak_dialog("WrongAnswer")
-
+    @intent_handler(IntentBuilder('AnswerIntent').require("answer1KeyWord"))
+    def handle_answer_intent(self, message):
+        if message.data["Answer"] == "नरेंद्र मोदी":
+            self.speak_dialog("CorrectAnswer")
+        else:
+            self.speak_dialog("WrongAnswer")
 
 def create_skill():
     return WinidoTestSkill()
